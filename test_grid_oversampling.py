6114454666e1b1a68400220e3b95b67d59b0e470
bryce-wedig <b.t.wedig@wustl.edu>
2023-12-08 16:21:53 -0600

    write sampling method test scripts

---
+++ test_grid_oversampling.py
@@ -0,0 +1,56 @@
+import os
+import sys
+import numpy as np
+from tqdm import tqdm
+
+from package.helpers import test_physical_lens
+from package.pandeia import pandeia_input
+from package.pandeia.pandeia_output import PandeiaOutput
+from package.helpers import pyhalo
+
+
+def main():
+    cwd = os.getcwd()
+    while os.path.basename(os.path.normpath(cwd)) != 'roman-pandeia':
+        cwd = os.path.dirname(cwd)
+    repo_path = cwd
+    if repo_path not in sys.path:
+        sys.path.append(repo_path)
+
+    csv = os.path.join(repo_path, 'data', 'roman_spacecraft_and_instrument_parameters.csv')
+    array_dir = os.path.join(repo_path, 'output', 'arrays', 'diagnostics', 'grid_oversampling')
+
+    num_samples = 10000
+    grid_oversample_list = [1, 3, 5, 7, 9]
+    execution_times = []
+
+    for grid_oversample in tqdm(grid_oversample_list):
+        # use test lens
+        lens = test_physical_lens.TestPhysicalLens()
+
+        # add CDM subhalos
+        lens.add_subhalos(*pyhalo.generate_CDM_halos(lens.z_lens, lens.z_source))
+
+        # generate lenstronomy model, varying grid oversample factor
+        model = lens.get_array(num_pix=45 * grid_oversample, side=5.)
+
+        # build Pandeia input
+        calc = pandeia_input.build_pandeia_calc(csv=csv,
+                                                array=model, 
+                                                lens=lens, 
+                                                band='f106', 
+                                                num_samples=num_samples)
+
+        # do Pandeia calculation        
+        image, execution_time = pandeia_input.get_pandeia_image(calc)
+        execution_times.append(execution_time)
+        
+        # save detector image
+        np.save(os.path.join(array_dir, f'grid_oversampling_{grid_oversample}'), image)
+
+    # save execution times
+    np.save(os.path.join(array_dir, 'execution_times_grid_oversampling.npy'), execution_times)
+
+
+if __name__ == '__main__':
+    main()
---
+++ test_number_samples.py
@@ -0,0 +1,56 @@
+import os
+import sys
+import numpy as np
+from tqdm import tqdm
+
+from package.helpers import test_physical_lens
+from package.pandeia import pandeia_input
+from package.pandeia.pandeia_output import PandeiaOutput
+from package.helpers import pyhalo
+
+
+def main():
+    cwd = os.getcwd()
+    while os.path.basename(os.path.normpath(cwd)) != 'roman-pandeia':
+        cwd = os.path.dirname(cwd)
+    repo_path = cwd
+    if repo_path not in sys.path:
+        sys.path.append(repo_path)
+
+    csv = os.path.join(repo_path, 'data', 'roman_spacecraft_and_instrument_parameters.csv')
+    array_dir = os.path.join(repo_path, 'output', 'arrays', 'diagnostics', 'num_samples')
+
+    num_samples_list = [10e2, 5*10e2, 10e3, 5*10e3, 10e4, 5*10e4, 10e5]
+    grid_oversample = 5
+    execution_times = []
+
+    for num_samples in tqdm(num_samples_list):
+        # use test lens
+        lens = test_physical_lens.TestPhysicalLens()
+
+        # add CDM subhalos
+        lens.add_subhalos(*pyhalo.generate_CDM_halos(lens.z_lens, lens.z_source))
+
+        # generate lenstronomy model, varying grid oversample factor
+        model = lens.get_array(num_pix=45 * grid_oversample, side=5.)
+
+        # build Pandeia input
+        calc = pandeia_input.build_pandeia_calc(csv=csv,
+                                                array=model, 
+                                                lens=lens, 
+                                                band='f106', 
+                                                num_samples=num_samples)
+
+        # do Pandeia calculation        
+        image, execution_time = pandeia_input.get_pandeia_image(calc)
+        execution_times.append(execution_time)
+        
+        # save detector image
+        np.save(os.path.join(array_dir, f'num_samples_{num_samples}'), image)
+
+    # save execution times
+    np.save(os.path.join(array_dir, 'execution_times_num_samples.npy'), execution_times)
+
+
+if __name__ == '__main__':
+    main()
