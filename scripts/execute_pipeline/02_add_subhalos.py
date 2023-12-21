62cc23931b8d9ce6f63e8dcd6c06bace6ddfe74e
bryce-wedig <b.t.wedig@wustl.edu>
2023-12-20 22:01:50 -0600

    work on color

--- scripts/execute_pipeline/02_add_subhalos.py
+++ scripts/execute_pipeline/02_add_subhalos.py
@@ -2,7 +2,6 @@ import multiprocessing
 import os
 import sys
 import time
-import traceback
 from multiprocessing import Pool
 
 import hydra
@@ -13,60 +12,55 @@ from tqdm import tqdm
 def main(config):
     start = time.time()
     
-    array_dir, data_dir, repo_dir, pickle_dir = config.machine.array_dir, config.machine.data_dir, config.machine.repo_dir, config.machine.pickle_dir
+    repo_dir, pickle_dir = config.machine.repo_dir, config.machine.pickle_dir
 
     # enable use of local packages
     if repo_dir not in sys.path:
         sys.path.append(repo_dir)
     from mejiro.utils import util
 
-    # TEMP: optionally, grab the first handful
-    # lens_list = lens_list[:100]
-
     # split up the lenses into batches based on core count
     cpu_count = multiprocessing.cpu_count()
-    process_count = int(cpu_count / 2)  # TODO consider making this larger, but using all CPUs has crashed
+    process_count = cpu_count - 4
 
-    # TODO need to make sure that the same substructure is being added to each filter
+    # get bands
+    bands = util.hydra_to_dict(config.pipeline)['band']
+
     # organize the pickles into a dict
     lens_dict = {}
-    for band in util.hydra_to_dict(config.pipeline)['band']:
+    for band in bands:
         # open pickled lens list
         lens_list = util.unpickle(os.path.join(pickle_dir, f'01_skypy_output_lens_list_{band.lower()}'))
         lens_dict[band] = lens_list
     
+    # TODO this naming is dumb, fix it
+    lenses = []
     # create a tuple for each lens
+    for i, _ in enumerate(lens_list):
+        lens = {}
+        for band in bands:
+            lens[band] = lens_dict[band][i]
+        lenses.append(lens)
 
-        
+    # directory to write the lenses with subhalos to
+    output_dir = os.path.join(pickle_dir, '02_lenses_with_substructure')
+    util.create_directory_if_not_exists(output_dir)
+    util.clear_directory(output_dir)
 
+    # tuple the parameters
+    pipeline_params = util.hydra_to_dict(config.pipeline)
+    tuple_list = []
+    for i, _ in enumerate(lenses):
+        tuple_list.append((lenses[i], pipeline_params, output_dir))
 
-    for band in util.hydra_to_dict(config.pipeline)['band']:
-        # directory to write the lenses with subhalos to
-        output_dir = os.path.join(pickle_dir, f'02_lenses_with_substructure_{band.lower()}')
-        util.create_directory_if_not_exists(output_dir)
-        util.clear_directory(output_dir)
+    # batch
+    generator = util.batch_list(tuple_list, process_count)
+    batches = list(generator)
 
-        
-
-        # tuple the parameters
-        pipeline_params = util.hydra_to_dict(config.pipeline)
-        tuple_list = []
-        for i, _ in enumerate(lens_list):
-            tuple_list.append((lens_list[i], pipeline_params, output_dir))
-
-        # batch
-        generator = util.batch_list(tuple_list, process_count)
-        batches = list(generator)
-
-        # process the batches
-        success_count = 0
-        for batch in tqdm(batches):
-            pool = Pool(processes=process_count)
-            for updated_lens in pool.map(add, batch):
-                if updated_lens is not None:
-                    success_count += 1
-                    
-        print(f'Added subhalos to {success_count} of {len(lens_list)} lenses')
+    # process the batches
+    for batch in tqdm(batches):
+        pool = Pool(processes=process_count)
+        pool.map(add, batch)
 
     stop = time.time()
     util.print_execution_time(start, stop)
@@ -82,24 +76,19 @@ def add(tuple):
     # unpack pipeline_params
     subhalo_cone = pipeline_params['subhalo_cone']
     los_normalization = pipeline_params['los_normalization']
+   
+    z_lens = round(list(lens.values())[0].z_lens, 2)
+    z_source = round(list(lens.values())[0].z_source, 2)
 
-    # add CDM subhalos
-    try:
-        # use same CDM subhalos
-        # lens.add_subhalos(*pyhalo.unpickle_subhalos('/data/bwedig/roman-pandeia/output/pickles/pyhalo/cdm_subhalos_tuple'))
+    # randomly generate CDM subhalos
+    halo_tuple = pyhalo.generate_CDM_halos(z_lens, z_source, cone_opening_angle_arcsec=subhalo_cone, LOS_normalization=los_normalization)
 
-        # TODO update with subhalo_cone param from hydra
+    # add this subhalo population to the lens for each filter
+    for band, band_lens in lens:
+        band_lens.add_subhalos(*halo_tuple)
 
-        # randomly generate CDM subhalos
-        lens.add_subhalos(*pyhalo.generate_CDM_halos(lens.z_lens, lens.z_source, cone_opening_angle_arcsec=subhalo_cone, LOS_normalization=los_normalization))
-        
-        # pickle
-        pickle_target = os.path.join(output_dir, f'lens_with_subhalos_{lens.uid}')
-        util.pickle(pickle_target, lens)
-        return 1
-    except:
-        # print(traceback.format_exc())
-        return None
+        pickle_target = os.path.join(output_dir, f'lens_with_subhalos_{lens.uid}_{band}')
+        util.pickle(pickle_target, band_lens)
 
 
 if __name__ == '__main__':
