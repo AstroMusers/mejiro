ffc0792699757bbc5fcf63bae94bc8a0e8450725
Bryce Wedig <84239923+bryce-wedig@users.noreply.github.com>
2023-12-24 11:52:58 -0500

    write pipeline setup script

--- scripts/execute_pipeline/00_pipeline_setup.py
+++ scripts/execute_pipeline/00_pipeline_setup.py
@@ -9,33 +9,23 @@ from glob import glob
 
 @hydra.main(version_base=None, config_path='../../config', config_name='config.yaml')
 def main(config):
-    start = time.time()
-
     array_dir, data_dir, repo_dir, pickle_dir = config.machine.array_dir, config.machine.data_dir, config.machine.repo_dir, config.machine.pickle_dir
 
-    # enable use of local packages
-    if repo_dir not in sys.path:
-        sys.path.append(repo_dir)
-    from mejiro.lenses import lens_util
-    from mejiro.utils import util
+    # check if mejiro in repo_dir
+    try:
+        if repo_dir not in sys.path:
+            sys.path.append(repo_dir)
+        import mejiro
+        module_path = os.path.dirname(mejiro.__file__)
+        print(f'Found mejiro at {module_path}')
+    except:
+        raise Exception(f'Could not find mejiro in {repo_dir}')
 
-    util.create_directory_if_not_exists(pickle_dir)
-        
-    # unpickle the lenses from the population survey and create lens objects
-    lens_dir = os.path.join('/data', 'bwedig', 'roman-population', 'data', 'lenses')
-    lens_paths = glob(lens_dir + '/*')
-    lens_list = []
-    for i, lens in tqdm(enumerate(lens_paths), total=len(lens_paths)):
-        lens = lens_util.unpickle_lens(lens, str(i).zfill(8))
-        lens_list.append(lens)
-
-    # pickle lens list
-    pickle_target = os.path.join(pickle_dir, '01_skypy_output_lens_list')
-    util.delete_if_exists(pickle_target)
-    util.pickle(pickle_target, lens_list)
-
-    stop = time.time()
-    util.print_execution_time(start, stop)
+    # check Pandeia installation
+    from pandeia import engine
+    print(engine.pandeia_version())
+    if 'ENVIRONMENT VARIABLE UNSET' in engine.pandeia_version():
+        raise Exception('Environment variable unset')
 
 
 if __name__ == '__main__':
