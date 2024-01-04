5dff25f18ecde3900295382c0fb5daab29d6b100
bryce-wedig <b.t.wedig@wustl.edu>
2024-01-03 20:32:52 -0600

    fix some color figures

--- mejiro/plots/overplot.py
+++ mejiro/plots/overplot.py
@@ -8,17 +8,17 @@ def text_boxes(ax, text_list, fontsize=18, alpha=0.5):
                   verticalalignment='top', bbox=props)
 
 
-def source_position(ax, lens):
+def source_position(ax, lens, linewidth=3):
     source_x, source_y = lens.get_source_pixel_coords()
-    return ax.scatter(source_x, source_y, edgecolor='purple', facecolor='none', s=150, label='Source position')
+    return ax.scatter(source_x, source_y, edgecolor='y', facecolor='none', s=150, label='Source position', linewidth=linewidth)
 
 
-def lens_position(ax, lens):
+def lens_position(ax, lens, linewidth=3):
     lens_x, lens_y = lens.get_lens_pixel_coords()
-    return ax.scatter(lens_x, lens_y, edgecolor='r', facecolor='none', s=150, label='Lens position')
+    return ax.scatter(lens_x, lens_y, edgecolor='r', facecolor='none', s=150, label='Lens position', linewidth=linewidth)
 
 
-def caustics(ax, lens):
+def caustics(ax, lens, linewidth=3):
     _, _, ra_caustic_list, dec_caustic_list = _get_caustics_critical_curves(lens)
 
     x_caustic_list, y_caustic_list = [], []
@@ -27,10 +27,10 @@ def caustics(ax, lens):
         x_caustic_list.append(x)
         y_caustic_list.append(y)
 
-    return ax.plot(x_caustic_list, y_caustic_list, label='Caustics', color='g')
+    return ax.plot(x_caustic_list, y_caustic_list, label='Caustics', color='g', linewidth=linewidth)
 
 
-def critical_curves(ax, lens):
+def critical_curves(ax, lens, linewidth=3):
     ra_critical_list, dec_critical_list, _, _ = _get_caustics_critical_curves(lens)
 
     x_critical_list, y_critical_list = [], []
@@ -39,7 +39,7 @@ def critical_curves(ax, lens):
         x_critical_list.append(x)
         y_critical_list.append(y)
 
-    return ax.plot(x_critical_list, y_critical_list, label='Critical curve', color='b')
+    return ax.plot(x_critical_list, y_critical_list, label='Critical curve', color='b', linewidth=linewidth)
 
 
 def _get_caustics_critical_curves(lens):
