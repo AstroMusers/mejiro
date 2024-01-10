0f157bf3a367972f74b2161ab32c868aa6ac862d
Bryce Wedig <84239923+bryce-wedig@users.noreply.github.com>
2024-01-10 17:23:42 -0600

    fix psf_compare script

--- paper/figures/psf_compare.py
+++ paper/figures/psf_compare.py
@@ -37,18 +37,8 @@ from mejiro.analysis import stats
 from mejiro.utils import util
 from mejiro.helpers import pyhalo, pandeia_input, psf
 
-
-# In[7]:
-
-
 array_dir = os.path.join(array_dir, 'sample_skypy_lens')
 pickle_dir = os.path.join(pickle_dir, 'pyhalo')
-
-
-# # Generate images with various PSFs
-
-# In[8]:
-
 
 lens = SampleSkyPyLens()
 
@@ -56,42 +46,16 @@ num_pix = 45
 side = 4.95
 band = 'f184'
 
-
-# ## Grab default substructure
-
-# In[9]:
-
-
 # add CDM subhalos; NB same subhalo population for all
 lens.add_subhalos(*pyhalo.unpickle_subhalos(os.path.join(pickle_dir, 'cdm_subhalos_tuple')))
-
-
-# ## No PSF
-
-# In[10]:
-
 
 no_psf_lens = deepcopy(lens)
 
 kwargs_psf_none = {'psf_type': 'NONE'}
-
 no_psf = no_psf_lens.get_array(num_pix=num_pix, kwargs_psf=kwargs_psf_none, side=side)
-
-plot.plot(title='No PSF',
-          array=no_psf,
-          colorbar=True)
-
 np.save(os.path.join(array_dir, 'no_psf.npy'), no_psf)
 
 print(no_psf.shape)
-
-
-# ## Gaussian PSF
-
-# I'm getting the PSF FWHM values from [here](https://roman.gsfc.nasa.gov/science/WFI_technical.html). From that webpage: "Note: PSF FWHM in arcseconds simulated at the center of the center pixel of a detector near the center of the WFI FOV using an input spectrum for a K0V type star."
-
-# In[11]:
-
 
 psf_fwhm = {
     'f062': 0.058,
@@ -104,31 +68,12 @@ psf_fwhm = {
     'f146': 0.105
 }
 
-
-# In[12]:
-
-
 gaussian_psf_lens = deepcopy(lens)
-
 kwargs_psf_gaussian = {'psf_type': 'GAUSSIAN', 'fwhm': psf_fwhm.get(band)}
-
 gaussian_psf = gaussian_psf_lens.get_array(num_pix=num_pix, kwargs_psf=kwargs_psf_gaussian, side=side)
-
-plot.plot(title='Gaussian PSF',
-          array=gaussian_psf,
-          colorbar=True)
-
 np.save(os.path.join(array_dir, 'gaussian_psf.npy'), gaussian_psf)
 
 print(gaussian_psf.shape)
-
-
-# ## WebbPSF
-
-# In[13]:
-
-
-import astropy.io.fits as pyfits
 
 # load PSF from WebbPSF
 wfi = roman.WFI()
@@ -139,34 +84,26 @@ calc_psf = wfi.calc_psf(oversample=5)
 kernel = psf.get_kernel_from_calc_psf(calc_psf)
 
 webbpsf_lens = deepcopy(lens)
-
 kwargs_webbpsf = {
     'psf_type': 'PIXEL',
     'kernel_point_source': kernel,
     'point_source_supersampling_factor': 5
     }
-
 webbpsf_psf = webbpsf_lens.get_array(num_pix=num_pix, kwargs_psf=kwargs_webbpsf, side=side)
-
-plot.plot(title='WebbPSF',
-          array=webbpsf_psf,
-          colorbar=True)
-
 np.save(os.path.join(array_dir, 'webbpsf.npy'), webbpsf_psf)
 
 print(webbpsf_psf.shape)
 
-no_psf = np.load(os.path.join(array_dir, 'no_psf.npy'))
-gaussian_psf = np.load(os.path.join(array_dir, 'gaussian_psf.npy'))
-pandeia = np.load(os.path.join(array_dir, 'webbpsf.npy'))
+# no_psf = np.load(os.path.join(array_dir, 'no_psf.npy'))
+# gaussian_psf = np.load(os.path.join(array_dir, 'gaussian_psf.npy'))
+# pandeia = np.load(os.path.join(array_dir, 'webbpsf.npy'))
 
 fig, ax = plt.subplots(nrows=2, ncols=3, figsize=(14, 8), gridspec_kw={'hspace': 0.1, 'wspace': 0.02})
 
 fontsize = 18
-
 matplotlib.rcParams.update({'font.size': fontsize})
 
-array_list = [no_psf, gaussian_psf, pandeia]
+array_list = [no_psf, gaussian_psf, webbpsf_psf]
 title_list = ['No PSF', f'Gaussian PSF: FWHM {psf_fwhm.get(band)}"', 'WebbPSF: F184']
 
 for i, array in enumerate(array_list):
@@ -177,18 +114,13 @@ for i, array in enumerate(array_list):
 cbar = fig.colorbar(axis_1, ax=ax[0])
 cbar.set_label('Counts/sec', rotation=90)
 
-# normalized_no_psf = stats.normalize(no_psf)
-# normalized_gaussian_psf = stats.normalize(gaussian_psf)
-# normalized_pandeia = stats.normalize(pandeia)
-
-# array_list = [normalized_pandeia - normalized_no_psf, normalized_pandeia - normalized_gaussian_psf, normalized_gaussian_psf - normalized_no_psf]
-array_list = [pandeia - no_psf, pandeia - gaussian_psf, gaussian_psf - no_psf]
+array_list = [webbpsf_psf - no_psf, webbpsf_psf - gaussian_psf, gaussian_psf - no_psf]
 title_list = ['WebbPSF - No PSF', 'WebbPSF - Gaussian PSF', 'Gaussian PSF - No PSF']
 
 # compute mean chi square
 chi_square_list = []
-chi_square_list.append(stats.chi_square(no_psf, pandeia))
-chi_square_list.append(stats.chi_square(gaussian_psf, pandeia))
+chi_square_list.append(stats.chi_square(no_psf, webbpsf_psf))
+chi_square_list.append(stats.chi_square(gaussian_psf, webbpsf_psf))
 chi_square_list.append(stats.chi_square(gaussian_psf, no_psf))
 chi_square_list = ['$\chi^2 = $' + '{:.2e}'.format(i) for i in chi_square_list]
 
@@ -204,22 +136,3 @@ overplot.text_boxes(ax[1], chi_square_list, fontsize=fontsize)
 fig.colorbar(axis_2, ax=ax[1], ticks=[-1, -10** -1, 0, 10 ** -1, 1])
 
 plt.savefig(os.path.join(figure_dir, 'psf_compare.png'))
-
-
-# In[17]:
-
-
-diagnostic_plot.residual(stats.normalize(pandeia), stats.normalize(no_psf))
-
-
-# In[18]:
-
-
-diagnostic_plot.residual(stats.normalize(pandeia), stats.normalize(gaussian_psf))
-
-
-# In[19]:
-
-
-diagnostic_plot.residual(pandeia - no_psf, pandeia - gaussian_psf)
-
