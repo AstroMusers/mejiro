c0025e21ac62b53034a4b9910eb288b208ddb73d
bryce-wedig <b.t.wedig@wustl.edu>
2024-04-23 09:06:17 -0500

    fix power spectra script

--- paper/supplemental/dev_generate_power_spectra.py
+++ paper/supplemental/dev_generate_power_spectra.py
@@ -39,7 +39,7 @@ def main(config):
 
     save_dir = os.path.join(config.machine.array_dir, 'power_spectra')
     util.create_directory_if_not_exists(save_dir)
-    util.clear_directory(save_dir)
+    # util.clear_directory(save_dir)
 
 
     # In[ ]:
@@ -74,64 +74,64 @@ def main(config):
     # In[ ]:
 
 
-    for lens in lens_list:
-        print(f'Processing lens {lens.uid}...')
-        z_lens = round(lens.z_lens, 2)
-        z_source = round(lens.z_source, 2)
-        log_m_host = np.log10(lens.main_halo_mass)
+    # for lens in lens_list:
+    #     print(f'Processing lens {lens.uid}...')
+    #     z_lens = round(lens.z_lens, 2)
+    #     z_source = round(lens.z_source, 2)
+    #     log_m_host = np.log10(lens.main_halo_mass)
 
-        cut_6 = CDM(z_lens,
-                    z_source,
-                    sigma_sub=sigma_sub,
-                    log_mlow=6.,
-                    log_mhigh=10.,
-                    log_m_host=log_m_host,
-                    r_tidal=r_tidal,
-                    cone_opening_angle_arcsec=subhalo_cone,
-                    LOS_normalization=los_normalization)
+    #     cut_6 = CDM(z_lens,
+    #                 z_source,
+    #                 sigma_sub=sigma_sub,
+    #                 log_mlow=6.,
+    #                 log_mhigh=10.,
+    #                 log_m_host=log_m_host,
+    #                 r_tidal=r_tidal,
+    #                 cone_opening_angle_arcsec=subhalo_cone,
+    #                 LOS_normalization=los_normalization)
 
-        cut_7 = CDM(z_lens,
-                    z_source,
-                    sigma_sub=sigma_sub,
-                    log_mlow=7.,
-                    log_mhigh=10.,
-                    log_m_host=log_m_host,
-                    r_tidal=r_tidal,
-                    cone_opening_angle_arcsec=subhalo_cone,
-                    LOS_normalization=los_normalization)
+    #     cut_7 = CDM(z_lens,
+    #                 z_source,
+    #                 sigma_sub=sigma_sub,
+    #                 log_mlow=7.,
+    #                 log_mhigh=10.,
+    #                 log_m_host=log_m_host,
+    #                 r_tidal=r_tidal,
+    #                 cone_opening_angle_arcsec=subhalo_cone,
+    #                 LOS_normalization=los_normalization)
 
-        cut_8 = CDM(z_lens,
-                    z_source,
-                    sigma_sub=sigma_sub,
-                    log_mlow=8.,
-                    log_mhigh=10.,
-                    log_m_host=log_m_host,
-                    r_tidal=r_tidal,
-                    cone_opening_angle_arcsec=subhalo_cone,
-                    LOS_normalization=los_normalization)
+    #     cut_8 = CDM(z_lens,
+    #                 z_source,
+    #                 sigma_sub=sigma_sub,
+    #                 log_mlow=8.,
+    #                 log_mhigh=10.,
+    #                 log_m_host=log_m_host,
+    #                 r_tidal=r_tidal,
+    #                 cone_opening_angle_arcsec=subhalo_cone,
+    #                 LOS_normalization=los_normalization)
 
-        lens_cut_6 = deepcopy(lens)
-        lens_cut_7 = deepcopy(lens)
-        lens_cut_8 = deepcopy(lens)
+    #     lens_cut_6 = deepcopy(lens)
+    #     lens_cut_7 = deepcopy(lens)
+    #     lens_cut_8 = deepcopy(lens)
 
-        lens_cut_6.add_subhalos(cut_6, suppress_output=True)
-        lens_cut_7.add_subhalos(cut_7, suppress_output=True)
-        lens_cut_8.add_subhalos(cut_8, suppress_output=True)
+    #     lens_cut_6.add_subhalos(cut_6, suppress_output=True)
+    #     lens_cut_7.add_subhalos(cut_7, suppress_output=True)
+    #     lens_cut_8.add_subhalos(cut_8, suppress_output=True)
 
-        lenses = [lens, lens_cut_6, lens_cut_7, lens_cut_8]
-        titles = [f'lens_{lens.uid}_no_subhalos', f'lens_{lens.uid}_cut_6', f'lens_{lens.uid}_cut_7',
-                    f'lens_{lens.uid}_cut_8']
-        models = [i.get_array(num_pix=num_pix * oversample, side=side, band='F106') for i in lenses]
+    #     lenses = [lens, lens_cut_6, lens_cut_7, lens_cut_8]
+    #     titles = [f'lens_{lens.uid}_no_subhalos', f'lens_{lens.uid}_cut_6', f'lens_{lens.uid}_cut_7',
+    #                 f'lens_{lens.uid}_cut_8']
+    #     models = [i.get_array(num_pix=num_pix * oversample, side=side, band='F106') for i in lenses]
 
-        for sl, model, title in zip(lenses, models, titles):
-            print(f'Processing model {title}...')
-            gs_images, _ = gs.get_images(sl, model, 'F106', input_size=num_pix, output_size=num_pix,
-                                        grid_oversample=oversample, psf_oversample=oversample,
-                                        detector=1, detector_pos=(2048, 2048), suppress_output=True)
-            ps, r = power_spectrum_1d(gs_images[0])
-            np.save(os.path.join(save_dir, f'im_subs_{title}.npy'), gs_images[0])
-            np.save(os.path.join(save_dir, f'ps_subs_{title}.npy'), ps)
-        np.save(os.path.join(save_dir, 'r.npy'), r)
+        # for sl, model, title in zip(lenses, models, titles):
+        #     print(f'Processing model {title}...')
+        #     gs_images, _ = gs.get_images(sl, model, 'F106', input_size=num_pix, output_size=num_pix,
+        #                                 grid_oversample=oversample, psf_oversample=oversample,
+        #                                 detector=1, detector_pos=(2048, 2048), suppress_output=True)
+        #     ps, r = power_spectrum_1d(gs_images[0])
+        #     np.save(os.path.join(save_dir, f'im_subs_{title}.npy'), gs_images[0])
+        #     np.save(os.path.join(save_dir, f'ps_subs_{title}.npy'), ps)
+        # np.save(os.path.join(save_dir, 'r.npy'), r)
 
         # for sl, model, title in zip(lenses, models, titles):
         #     print(f'    Processing model {title} kappa...')
@@ -155,29 +155,34 @@ def main(config):
         z_source = round(lens.z_source, 2)
         log_m_host = np.log10(lens.main_halo_mass)
 
-        cut_6 = CDM(z_lens,
-                    z_source,
-                    sigma_sub=sigma_sub,
-                    log_mlow=6.,
-                    log_mhigh=10.,
-                    log_m_host=log_m_host,
-                    r_tidal=r_tidal,
-                    cone_opening_angle_arcsec=subhalo_cone,
-                    LOS_normalization=los_normalization)
+        try:
+            cut_6 = CDM(z_lens,
+                        z_source,
+                        sigma_sub=sigma_sub,
+                        log_mlow=6.,
+                        log_mhigh=10.,
+                        log_m_host=log_m_host,
+                        r_tidal=r_tidal,
+                        cone_opening_angle_arcsec=subhalo_cone,
+                        LOS_normalization=los_normalization)
+        except:
+            continue
         
-        lens_cut_6.add_subhalos(cut_6, suppress_output=True)
+        lens.add_subhalos(cut_6, suppress_output=True)
+
+        model = lens.get_array(num_pix=num_pix * oversample, side=side, band='F106')
         
         detectors = [4, 1, 9, 17]
         detector_positions = [(4, 4092), (2048, 2048), (4, 4), (4092, 4092)]
 
         for detector, detector_pos in zip(detectors, detector_positions):
             print(f'Processing detector {detector}, {detector_pos}...')
-            gs_images, _ = gs.get_images(sl, model, 'F106', input_size=num_pix, output_size=num_pix,
+            gs_images, _ = gs.get_images(lens, model, 'F106', input_size=num_pix, output_size=num_pix,
                                         grid_oversample=oversample, psf_oversample=oversample,
                                         detector=detector, detector_pos=detector_pos, suppress_output=True)
             ps, r = power_spectrum_1d(gs_images[0])
-            np.save(os.path.join(save_dir, f'im_det_{detector}.npy'), gs_images[0])
-            np.save(os.path.join(save_dir, f'ps_det_{detector}.npy'), ps)
+            np.save(os.path.join(save_dir, f'im_det_{detector}_{lens.uid}.npy'), gs_images[0])
+            np.save(os.path.join(save_dir, f'ps_det_{detector}_{lens.uid}.npy'), ps)
 
 
 if __name__ == '__main__':
