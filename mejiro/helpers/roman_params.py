6fb647c667b338e0b04356798bca22554c3967e2
bryce-wedig <b.t.wedig@wustl.edu>
2024-06-26 12:43:11 -0500

    work on hwo figures

--- mejiro/helpers/roman_params.py
+++ mejiro/helpers/roman_params.py
@@ -9,8 +9,6 @@ class RomanParameters:
     def __init__(self, csv):
         self.df = pd.read_csv(csv)
 
-    # TODO get FWHM for each filter
-
     def get_filter_centers(self):
         roman_filters = ['F062', 'F087', 'F106', 'F129', 'F158', 'F184', 'F213', 'F146']
         fields = [f'WFI_Filter_{filter}_Center' for filter in roman_filters]
