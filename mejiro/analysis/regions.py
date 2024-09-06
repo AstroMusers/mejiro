d18cd5613e47325cd0465d503032b7adf1cebcf5
bryce-wedig <b.t.wedig@wustl.edu>
2024-09-05 20:49:41 -0500

    work on optimizing region-identifying code

--- mejiro/analysis/regions.py
+++ mejiro/analysis/regions.py
@@ -7,6 +7,50 @@ import numpy as np
 from mejiro.utils import util
 
 
+def remove_single_pixels(masked_array):
+    x_shape, y_shape = masked_array.shape
+
+    masked_array[0, 10] = 1
+    masked_array[0, 0] = 1
+    masked_array[x_shape - 1, y_shape - 1] = 1
+
+    coords = []
+    for x in range(0, x_shape):
+        for y in range(0, y_shape):
+            if masked_array[x, y] != 0:
+                if x == 0 and y == 0:  # bottom left corner
+                    if masked_array[x + 1, y] == 0 and masked_array[x, y + 1] == 0:
+                        coords.append((x, y))
+                elif x == 0 and y == y_shape - 1:  # bottom right corner
+                    if masked_array[x + 1, y] == 0 and masked_array[x, y - 1] == 0:
+                        coords.append((x, y))
+                elif x == x_shape and y == 0:  # top left corner
+                    if masked_array[x - 1, y] == 0 and masked_array[x, y + 1] == 0:
+                        coords.append((x, y))
+                elif x == x_shape - 1 and y == y_shape - 1:  # top right corner
+                    if masked_array[x - 1, y] == 0 and masked_array[x, y - 1] == 0:
+                        coords.append((x, y))
+                elif x == 0:  # bottom edge
+                    if masked_array[x - 1, y] == 0 and masked_array[x + 1, y] == 0 and masked_array[x, y + 1] == 0:
+                        coords.append((x, y))
+                elif x == x_shape - 1:  # top edge
+                    if masked_array[x - 1, y] == 0 and masked_array[x + 1, y] == 0 and masked_array[x, y - 1] == 0 and masked_array[x, y + 1] == 0:
+                        coords.append((x, y))
+                elif y == 0:  # left edge
+                    if masked_array[x - 1, y] == 0 and masked_array[x + 1, y] == 0 and masked_array[x, y - 1] == 0 and masked_array[x, y + 1] == 0:
+                        coords.append((x, y))
+                elif y == y_shape - 1:  # right edge
+                    if masked_array[x - 1, y] == 0 and masked_array[x + 1, y] == 0 and masked_array[x, y - 1] == 0:
+                        coords.append((x, y))
+                elif masked_array[x - 1, y] == 0 and masked_array[x + 1, y] == 0 and masked_array[x, y - 1] == 0 and masked_array[x, y + 1] == 0:
+                    coords.append((x, y))
+
+    for coord in coords:
+        masked_array[coord] = 0
+
+    return masked_array
+
+
 def annular_mask(dimx, dimy, center, r_in, r_out):
     Y, X = np.ogrid[:dimx, :dimy]
     distance_from_center = np.sqrt((X - center[0]) ** 2 + (Y - center[1]) ** 2)
