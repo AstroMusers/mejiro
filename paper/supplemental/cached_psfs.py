1b06601d0704cbf6e5d5c4d1f46929bb49d18fce
bryce-wedig <b.t.wedig@wustl.edu>
2024-09-17 17:02:49 -0500

    cache psfs for ldsm with num_pix param for psf

--- paper/supplemental/cached_psfs.py
+++ paper/supplemental/cached_psfs.py
@@ -26,28 +26,32 @@ def main(config):
     save_dir = os.path.join(config.machine.data_dir, 'cached_psfs')
     util.create_directory_if_not_exists(save_dir)
 
-    oversamples = [1]
+    oversamples = [1, 3, 5]
     # bands = ['F106', 'F129', 'F158', 'F184']
-    bands = ['F062']
+    bands = ['F087']
     # detectors = [4, 1, 9, 17]
     # detector_positions = [(4, 4092), (2048, 2048), (4, 4), (4092, 4092)]
     # detectors = [1, 2, 4, 5]
     # detector_positions = [(2048, 2048), (2048, 2048), (2048, 2048), (2048, 2048)]
     detectors = list(range(1, 19))
     # detector_positions = [(2048, 2048)] * 18
-    detector_positions = Roman().divide_up_sca(2)
+    detector_positions = []
+    for i in range(4):
+        detector_positions.extend(Roman().divide_up_sca(i + 1))
+    num_pixes = [47, 141, 235]
 
-    for oversample in oversamples:
+    num_iterations = len(oversamples) * len(bands) * len(detectors) * len(detector_positions)
+    for oversample, num_pix in zip(oversamples, num_pixes):
         for band in bands:
             for detector in tqdm(detectors, leave=True):
                 for detector_position in tqdm(detector_positions, leave=False):
-                    psf_filename = f'{band}_{detector}_{detector_position[0]}_{detector_position[1]}_{oversample}.pkl'
+                    psf_filename = f'{psf.get_psf_id_string(band, detector, detector_position, oversample, num_pix)}.pkl'
                     psf_path = os.path.join(save_dir, psf_filename)
                     if os.path.exists(psf_path):
                         print(f'{psf_path} already exists')
                         continue
                     else:
-                        webbpsf_psf = psf.get_webbpsf_psf(band, detector, detector_position, oversample)
+                        webbpsf_psf = psf.get_webbpsf_psf(band, detector, detector_position, oversample, num_pix)
                         util.pickle(psf_path, webbpsf_psf)
                         print(f'Pickled {psf_path}')
 
