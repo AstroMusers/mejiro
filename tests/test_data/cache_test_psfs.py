f2fb2c6e5eeea76c19937e223a33da2bab70ccb4
bryce-wedig <b.t.wedig@wustl.edu>
2024-09-25 09:06:15 -0500

    write script to cache test PSFs

---
+++ tests/test_data/cache_test_psfs.py
@@ -0,0 +1,22 @@
+import sys
+
+import hydra
+
+
+@hydra.main(version_base=None, config_path='../../config', config_name='config.yaml')
+def main(config):
+    # enable use of local packages
+    repo_dir = config.machine.repo_dir
+    if repo_dir not in sys.path:
+        sys.path.append(repo_dir)
+    from mejiro.engines import webbpsf_engine
+
+    bands = ['F062', 'F087', 'F106', 'F129', 'F146', 'F158', 'F184', 'F213']
+
+    for band in bands:
+        psf_id = webbpsf_engine.get_psf_id(band, 1, (2048, 2048), 1, 47)
+        webbpsf_engine.cache_psf(psf_id, '')
+
+
+if __name__ == '__main__':
+    main()
