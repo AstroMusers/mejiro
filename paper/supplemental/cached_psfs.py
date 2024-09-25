6edd07b497ae02b2fd3b756066ccd7d2ed153a56
bryce-wedig <b.t.wedig@wustl.edu>
2024-09-25 17:04:21 -0500

    cache centered PSFs for each SCA

--- paper/supplemental/cached_psfs.py
+++ paper/supplemental/cached_psfs.py
@@ -32,18 +32,18 @@ def main(config):
 
     oversamples = [1]
     # bands = ['F106', 'F129', 'F158', 'F184']
-    bands = ['F087']
+    bands = ['F129']
     # detectors = [4, 1, 9, 17]
     # detector_positions = [(4, 4092), (2048, 2048), (4, 4), (4092, 4092)]
     # detectors = [1, 2, 4, 5]
     # detector_positions = [(2048, 2048), (2048, 2048), (2048, 2048), (2048, 2048)]
     detectors = list(range(1, 19))
-    # detector_positions = [(2048, 2048)] * 18
+    detector_positions = [(2048, 2048)]
     # detector_positions = []
     # for i in range(4):
     #     detector_positions.extend(roman_util.divide_up_sca(i + 1))
-    detector_positions = roman_util.divide_up_sca(3)
-    num_pixes = [97]
+    # detector_positions = roman_util.divide_up_sca(3)
+    num_pixes = [47]
 
     # determine which PSFs need to be generated
     psf_id_strings = []
