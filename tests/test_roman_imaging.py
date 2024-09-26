8206b886412e3d319ecffd6b97c41fe9ea6470cc
bryce-wedig <b.t.wedig@wustl.edu>
2024-09-25 21:12:06 -0500

    write more Roman imaging tests

--- tests/test_roman_imaging.py
+++ tests/test_roman_imaging.py
@@ -6,28 +6,28 @@ from mejiro.exposure import Exposure
 from mejiro.lenses.test import SampleStrongLens
 
 def test_roman_imaging():
+    """
+    Simplest case
+    """
     roman = Roman()
-
     lens = SampleStrongLens()
     band = 'F129'
     scene_size = 5  # arcsec
     oversample = 1
     exposure_time = 146
-    instrument_params = {
-        'detector': 1,
-        'detector_position': (2048, 2048)
-    }
 
     synthetic_image = SyntheticImage(strong_lens=lens, 
                                      instrument=roman, 
                                      band=band, 
                                      arcsec=scene_size, 
                                      oversample=oversample, 
-                                     instrument_params=instrument_params,
                                      verbose=False)
     
     exposure = Exposure(synthetic_image, 
-                        exposure_time=exposure_time, verbose=False)
+                        exposure_time=exposure_time, 
+                        check_cache=True,
+                        psf_cache_dir='test_data',
+                        verbose=False)
     
     assert synthetic_image.pixel_scale == 0.11
     assert synthetic_image.native_pixel_scale == 0.11
@@ -35,31 +35,33 @@ def test_roman_imaging():
     assert synthetic_image.native_num_pix == 47
     assert synthetic_image.arcsec == 5.17
     assert synthetic_image.image.shape == (47, 47)
+
+    # TODO checks on the images
     
 
 def test_roman_oversampled_imaging():
+    """
+    Oversample the image by an odd factor -- another common use case
+    """
     roman = Roman()
-
     lens = SampleStrongLens()
     band = 'F129'
     scene_size = 5  # arcsec
     oversample = 5
     exposure_time = 146
-    instrument_params = {
-        'detector': 1,
-        'detector_position': (2048, 2048)
-    }
 
     synthetic_image = SyntheticImage(strong_lens=lens, 
                                      instrument=roman, 
                                      band=band, 
                                      arcsec=scene_size, 
                                      oversample=oversample, 
-                                     instrument_params=instrument_params,
                                      verbose=False)
     
     exposure = Exposure(synthetic_image, 
-                        exposure_time=exposure_time, verbose=False)
+                        exposure_time=exposure_time, 
+                        check_cache=True,
+                        psf_cache_dir='test_data',
+                        verbose=False)
     
     assert synthetic_image.pixel_scale == 0.022
     assert synthetic_image.native_pixel_scale == 0.11
@@ -68,5 +70,293 @@ def test_roman_oversampled_imaging():
     assert synthetic_image.arcsec == 5.17
     assert synthetic_image.image.shape == (235, 235)
 
-# TODO test pieces
+    # TODO checks on the images
+
+
+def test_roman_generate_psf():
+    """
+    Provide a detector position by overriding the default instrument params where the PSF at that position is not cached, so the WebbPSF engine is automatically called to generate it
+    """
+    roman = Roman()
+    lens = SampleStrongLens()
+    band = 'F129'
+    scene_size = 5  # arcsec
+    oversample = 1
+    exposure_time = 146
+    instrument_params = {
+        'detector': 1,
+        'detector_position': (2047, 2047)
+    }
+
+    synthetic_image = SyntheticImage(strong_lens=lens, 
+                                     instrument=roman, 
+                                     band=band, 
+                                     arcsec=scene_size, 
+                                     oversample=oversample, 
+                                     instrument_params=instrument_params,
+                                     verbose=False)
     
+    with pytest.warns(UserWarning, match='PSF .* not found in cache .*'):
+        exposure = Exposure(synthetic_image, 
+                            exposure_time=exposure_time, 
+                            verbose=False)
+        
+    # TODO checks on the images
+    
+
+def test_roman_cached_psf():
+    roman = Roman()
+    lens = SampleStrongLens()
+    band = 'F129'
+    scene_size = 5  # arcsec
+    oversample = 1
+    exposure_time = 146
+
+    synthetic_image = SyntheticImage(strong_lens=lens, 
+                                     instrument=roman, 
+                                     band=band, 
+                                     arcsec=scene_size, 
+                                     oversample=oversample, 
+                                     verbose=False)
+    
+    import numpy as np
+    psf = np.load('test_data/F129_1_2048_2048_1_47.npy')
+    
+    exposure = Exposure(synthetic_image, 
+                        exposure_time=exposure_time, 
+                        psf=psf,
+                        verbose=False)
+    
+    # TODO checks on the images
+
+    
+def test_roman_sky_background_off():
+    roman = Roman()
+    lens = SampleStrongLens()
+    band = 'F129'
+    scene_size = 5  # arcsec
+    oversample = 1
+    exposure_time = 146
+
+    synthetic_image = SyntheticImage(strong_lens=lens, 
+                                     instrument=roman, 
+                                     band=band, 
+                                     arcsec=scene_size, 
+                                     oversample=oversample, 
+                                     verbose=False)
+    
+    engine_params = {
+        'sky_background': False
+    }
+    
+    exposure = Exposure(synthetic_image, 
+                        exposure_time=exposure_time, 
+                        engine_params=engine_params,
+                        check_cache=True,
+                        psf_cache_dir='test_data',
+                        verbose=False)
+    
+    # TODO checks on the images
+
+
+def test_roman_all_detector_effects_off():
+    roman = Roman()
+    lens = SampleStrongLens()
+    band = 'F129'
+    scene_size = 5  # arcsec
+    oversample = 1
+    exposure_time = 146
+
+    synthetic_image = SyntheticImage(strong_lens=lens, 
+                                     instrument=roman, 
+                                     band=band, 
+                                     arcsec=scene_size, 
+                                     oversample=oversample, 
+                                     verbose=False)
+    
+    engine_params = {
+        'detector_effects': False
+    }
+    
+    exposure = Exposure(synthetic_image, 
+                        exposure_time=exposure_time, 
+                        engine_params=engine_params,
+                        check_cache=True,
+                        psf_cache_dir='test_data',
+                        verbose=False)
+    
+    # TODO checks on the images
+    
+
+def test_roman_poisson_noise_off():
+    roman = Roman()
+    lens = SampleStrongLens()
+    band = 'F129'
+    scene_size = 5  # arcsec
+    oversample = 1
+    exposure_time = 146
+
+    synthetic_image = SyntheticImage(strong_lens=lens, 
+                                     instrument=roman, 
+                                     band=band, 
+                                     arcsec=scene_size, 
+                                     oversample=oversample, 
+                                     verbose=False)
+    
+    engine_params = {
+        'poisson_noise': False
+    }
+    
+    exposure = Exposure(synthetic_image, 
+                        exposure_time=exposure_time, 
+                        engine_params=engine_params,
+                        check_cache=True,
+                        psf_cache_dir='test_data',
+                        verbose=False)
+    
+    # TODO checks on the images
+    
+
+def test_roman_reciprocity_failure_off():
+    roman = Roman()
+    lens = SampleStrongLens()
+    band = 'F129'
+    scene_size = 5  # arcsec
+    oversample = 1
+    exposure_time = 146
+
+    synthetic_image = SyntheticImage(strong_lens=lens, 
+                                     instrument=roman, 
+                                     band=band, 
+                                     arcsec=scene_size, 
+                                     oversample=oversample, 
+                                     verbose=False)
+    
+    engine_params = {
+        'reciprocity_failure': False
+    }
+    
+    exposure = Exposure(synthetic_image, 
+                        exposure_time=exposure_time, 
+                        engine_params=engine_params,
+                        check_cache=True,
+                        psf_cache_dir='test_data',
+                        verbose=False)
+    
+    # TODO checks on the images
+    
+
+def test_roman_dark_noise_off():
+    roman = Roman()
+    lens = SampleStrongLens()
+    band = 'F129'
+    scene_size = 5  # arcsec
+    oversample = 1
+    exposure_time = 146
+
+    synthetic_image = SyntheticImage(strong_lens=lens, 
+                                     instrument=roman, 
+                                     band=band, 
+                                     arcsec=scene_size, 
+                                     oversample=oversample, 
+                                     verbose=False)
+    
+    engine_params = {
+        'dark_noise': False
+    }
+    
+    exposure = Exposure(synthetic_image, 
+                        exposure_time=exposure_time, 
+                        engine_params=engine_params,
+                        check_cache=True,
+                        psf_cache_dir='test_data',
+                        verbose=False)
+    
+    # TODO checks on the images
+    
+
+def test_roman_nonlinearity_off():
+    roman = Roman()
+    lens = SampleStrongLens()
+    band = 'F129'
+    scene_size = 5  # arcsec
+    oversample = 1
+    exposure_time = 146
+
+    synthetic_image = SyntheticImage(strong_lens=lens, 
+                                     instrument=roman, 
+                                     band=band, 
+                                     arcsec=scene_size, 
+                                     oversample=oversample, 
+                                     verbose=False)
+    
+    engine_params = {
+        'nonlinearity': False
+    }
+    
+    exposure = Exposure(synthetic_image, 
+                        exposure_time=exposure_time, 
+                        engine_params=engine_params,
+                        check_cache=True,
+                        psf_cache_dir='test_data',
+                        verbose=False)
+    
+    # TODO checks on the images
+    
+
+def test_roman_ipc_off():
+    roman = Roman()
+    lens = SampleStrongLens()
+    band = 'F129'
+    scene_size = 5  # arcsec
+    oversample = 1
+    exposure_time = 146
+
+    synthetic_image = SyntheticImage(strong_lens=lens, 
+                                     instrument=roman, 
+                                     band=band, 
+                                     arcsec=scene_size, 
+                                     oversample=oversample, 
+                                     verbose=False)
+    
+    engine_params = {
+        'ipc': False
+    }
+    
+    exposure = Exposure(synthetic_image, 
+                        exposure_time=exposure_time, 
+                        engine_params=engine_params,
+                        check_cache=True,
+                        psf_cache_dir='test_data',
+                        verbose=False)
+    
+    # TODO checks on the images
+    
+
+def test_roman_read_noise_off():
+    roman = Roman()
+    lens = SampleStrongLens()
+    band = 'F129'
+    scene_size = 5  # arcsec
+    oversample = 1
+    exposure_time = 146
+
+    synthetic_image = SyntheticImage(strong_lens=lens, 
+                                     instrument=roman, 
+                                     band=band, 
+                                     arcsec=scene_size, 
+                                     oversample=oversample, 
+                                     verbose=False)
+    
+    engine_params = {
+        'read_noise': False
+    }
+    
+    exposure = Exposure(synthetic_image, 
+                        exposure_time=exposure_time, 
+                        engine_params=engine_params,
+                        check_cache=True,
+                        psf_cache_dir='test_data',
+                        verbose=False)
+    
+    # TODO checks on the images
