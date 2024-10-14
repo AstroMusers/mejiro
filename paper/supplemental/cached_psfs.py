75a92656189ebbdadc02b83f027f113c6d4cb9bc
bryce-wedig <b.t.wedig@wustl.edu>
2024-10-13 21:43:42 -0500

    cache PSFs as npy instead of pickled InterpolatedImage

--- paper/supplemental/cached_psfs.py
+++ paper/supplemental/cached_psfs.py
@@ -3,6 +3,7 @@ import os
 import sys
 import time
 from multiprocessing import Pool
+import numpy as np
 
 import hydra
 from hydra.core.hydra_config import HydraConfig
@@ -19,7 +20,7 @@ def main(config):
     if config.machine.repo_dir not in sys.path:
         sys.path.append(config.machine.repo_dir)
     from mejiro.utils import roman_util, util
-    from mejiro.helpers import psf
+    from mejiro.engines import webbpsf_engine
 
     machine = HydraConfig.get().runtime.choices.machine
     if machine == 'hpc':
@@ -31,42 +32,43 @@ def main(config):
     # save_dir = os.path.join(config.machine.repo_dir, 'mejiro', 'data', 'cached_psfs')
     save_dir = os.path.join(config.machine.data_dir, 'cached_psfs')
     util.create_directory_if_not_exists(save_dir)
+    print(f'Saving PSFs to {save_dir}')
 
-    oversamples = [1]
+    oversamples = [5]
     # bands = ['F087']  # , 'F106', 'F129', 'F158', 'F184'
-    bands = ['F129']
+    bands = ['F087']
     # detectors = [4, 1, 9, 17]
     # detector_positions = [(4, 4092), (2048, 2048), (4, 4), (4092, 4092)]
     # detectors = [1, 2, 4, 5]
     # detector_positions = [(2048, 2048), (2048, 2048), (2048, 2048), (2048, 2048)]
     detectors = list(range(1, 19))
-    detector_positions = [(2048, 2048)]
+    # detector_positions = [(2048, 2048)] * 18
     # detector_positions = []
     # for i in range(4):
     #     detector_positions.extend(roman_util.divide_up_sca(i + 1))
-    # detector_positions = roman_util.divide_up_sca(2)
+    detector_positions = roman_util.divide_up_sca(2)
     num_pixes = [101]
 
     # determine which PSFs need to be generated
-    psf_id_strings = []
+    psf_ids = []
     for oversample, num_pix in zip(oversamples, num_pixes):
         for band in bands:
             for detector in detectors:
                 for detector_position in detector_positions:
-                    psf_id_string = psf.get_psf_id_string(band, detector, detector_position, oversample, num_pix)
-                    psf_filename = f'{psf_id_string}.pkl'
+                    psf_id = webbpsf_engine.get_psf_id(band, detector, detector_position, oversample, num_pix)
+                    psf_filename = f'{psf_id}.npy'
                     psf_path = os.path.join(save_dir, psf_filename)
                     if os.path.exists(psf_path):
                         print(f'{psf_path} already exists')
                         continue
                     else:
-                        psf_id_strings.append(psf_id_string)
+                        psf_ids.append(psf_id)
 
-    if len(psf_id_strings) == 0:
+    if len(psf_ids) == 0:
         print('All PSFs already exist. Exiting.')
         return
 
-    arg_list = [(psf_id_string, save_dir) for psf_id_string in psf_id_strings]
+    arg_list = [(psf_id_string, save_dir) for psf_id_string in psf_ids]
 
     # split up the lenses into batches based on core count
     count = len(arg_list)
@@ -91,18 +93,17 @@ def main(config):
 
 
 def generate_psf(tuple):
-    from mejiro.utils import util
-    from mejiro.helpers import psf
+    from mejiro.engines import webbpsf_engine
 
     # unpack tuple
-    (psf_id_string, save_dir) = tuple
+    (psf_id, save_dir) = tuple
 
     # generate PSF
-    webbpsf_psf = psf.get_webbpsf_psf_from_string(psf_id_string)
+    webbpsf_psf = webbpsf_engine.get_roman_psf_from_id(psf_id, check_cache=False, verbose=False)
 
     # pickle PSF
-    psf_path = os.path.join(save_dir, f'{psf_id_string}.pkl')
-    util.pickle(psf_path, webbpsf_psf)
+    psf_path = os.path.join(save_dir, f'{psf_id}.npy')
+    np.save(psf_path, webbpsf_psf)
     # print(f'Pickled {psf_path}')
 
 
