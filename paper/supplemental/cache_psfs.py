fd3e58e6e5a67634c6accc2494648814a4d99f82
bryce-wedig <b.t.wedig@wustl.edu>
2024-10-19 12:41:46 -0500

    write ProcessPoolExecutor version of cache PSFs script

--- paper/supplemental/cache_psfs.py
+++ paper/supplemental/cache_psfs.py
@@ -2,7 +2,7 @@ import multiprocessing
 import os
 import sys
 import time
-from multiprocessing import Pool
+from concurrent.futures import ProcessPoolExecutor, as_completed
 import numpy as np
 
 import hydra
@@ -16,7 +16,7 @@ def main(config):
 
     os.nice(19)
 
-    # enable use of local packages
+    # Enable use of local packages
     if config.machine.repo_dir not in sys.path:
         sys.path.append(config.machine.repo_dir)
     from mejiro.utils import roman_util, util
@@ -29,8 +29,7 @@ def main(config):
     elif machine == 'uzay':
         os.environ['WEBBPSF_PATH'] = '/data/scratch/btwedig/STScI/ref_data/webbpsf-data'
 
-    # set directory for all output of this script
-    # save_dir = os.path.join(config.machine.repo_dir, 'mejiro', 'data', 'cached_psfs')
+    # Set directory for all output of this script
     save_dir = os.path.join(config.machine.data_dir, 'cached_psfs')
     util.create_directory_if_not_exists(save_dir)
     print(f'Saving PSFs to {save_dir}')
@@ -50,7 +49,7 @@ def main(config):
     # detector_positions = roman_util.divide_up_sca(4)
     num_pixes = [101]
 
-    # determine which PSFs need to be generated
+    # Determine which PSFs need to be generated
     psf_ids = []
     for oversample, num_pix in zip(oversamples, num_pixes):
         for band in bands:
@@ -71,41 +70,37 @@ def main(config):
 
     arg_list = [(psf_id_string, save_dir) for psf_id_string in psf_ids]
 
-    # split up the lenses into batches based on core count
+    # Determine the number of processes
     count = len(arg_list)
     cpu_count = multiprocessing.cpu_count()
-    # process_count = cpu_count - config.machine.headroom_cores
-    # if count < process_count:
-    #     process_count = count
-    process_count = 10
+    process_count = 10  # Fixed process count (you can adjust this as needed)
     print(f'Spinning up {process_count} process(es) on {cpu_count} core(s) to generate {count} PSF(s)')
 
-    # batch
-    generator = util.batch_list(arg_list, process_count)
-    batches = list(generator)
+    # Process the tasks with ProcessPoolExecutor
+    with ProcessPoolExecutor(max_workers=process_count) as executor:
+        futures = [executor.submit(generate_psf, args) for args in arg_list]
 
-    # process the batches
-    for batch in tqdm(batches):
-        pool = Pool(processes=process_count)
-        pool.map(generate_psf, batch)
+        for future in tqdm(as_completed(futures), total=len(futures)):
+            future.result()  # Get the result to propagate exceptions if any
 
     stop = time.time()
     util.print_execution_time(start, stop)
 
 
-def generate_psf(tuple):
+def generate_psf(args):
     from mejiro.engines import webbpsf_engine
 
-    # unpack tuple
-    (psf_id, save_dir) = tuple
+    # Unpack tuple
+    psf_id, save_dir = args
 
-    # generate PSF
+    # Generate PSF
     webbpsf_psf = webbpsf_engine.get_roman_psf_from_id(psf_id, check_cache=False, verbose=False)
 
-    # pickle PSF
+    # Save PSF
     psf_path = os.path.join(save_dir, f'{psf_id}.npy')
     np.save(psf_path, webbpsf_psf)
-    # print(f'Pickled {psf_path}')
+    # Print statement optional for debugging purposes
+    # print(f'Saved {psf_path}')
 
 
 if __name__ == '__main__':
