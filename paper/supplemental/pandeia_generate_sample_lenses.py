b552800429d7208baa002c2c69109f0a85601dbe
bryce-wedig <b.t.wedig@wustl.edu>
2024-10-28 13:45:46 -0500

    review results of Pandeia calc param tests

--- paper/supplemental/pandeia_generate_sample_lenses.py
+++ paper/supplemental/pandeia_generate_sample_lenses.py
@@ -7,7 +7,7 @@ import numpy as np
 from tqdm import tqdm
 
 
-@hydra.main(version_base=None, config_path='../config', config_name='config.yaml')
+@hydra.main(version_base=None, config_path='../../config', config_name='config.yaml')
 def main(config):
     array_dir, pickle_dir, repo_dir = config.machine.array_dir, config.machine.pickle_dir, config.machine.repo_dir
 
@@ -19,8 +19,8 @@ def main(config):
     from mejiro.utils import util
 
     output_dir = os.path.join(array_dir, 'sample_skypy_lens')
-    util.create_directory_if_not_exists(output_dir)
-    util.clear_directory(output_dir)
+    # util.create_directory_if_not_exists(output_dir)
+    # util.clear_directory(output_dir)
 
     # enable use of local packages
     if repo_dir not in sys.path:
@@ -30,8 +30,10 @@ def main(config):
     num_pix = 51
     side = 5.61
     output_size = 45
-    grid_oversample_list = [1, 3, 5]
-    num_samples_list = [100, 1000, 10000, 100000, 1000000, 10000000]  # 
+    # grid_oversample_list = [1, 3, 5]  # 
+    grid_oversample_list = [1]
+    # num_samples_list = [100, 1000, 10000, 100000, 1000000]  # 10000000
+    num_samples_list = [int(1e7), int(1e8), int(1e9)]
 
     # use test lens
     lens = SampleStrongLens()
