3a7d4126beab6cbcd3f421f6d74e4fab8727f827
bryce-wedig <b.t.wedig@wustl.edu>
2024-11-12 17:25:07 -0500

    increment version for next release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='mejiro',
     packages=find_packages(),
-    version='0.0.1',
+    version='0.0.2',
     author='Bryce Wedig',
     url='https://github.com/AstroMusers/mejiro',
     license='MIT'
