fc1c2174ffebc85c11029dc8423dd13365e96a53
Bryce Wedig <84239923+bryce-wedig@users.noreply.github.com>
2024-12-25 22:43:31 -0500

    execute script 00 with updated dependencies and yaml config

--- mejiro/lenses/lens_util.py
+++ mejiro/lenses/lens_util.py
@@ -142,10 +142,10 @@ def slsim_lens_to_mejiro(slsim_lens, bands, cosmo, snr=None, uid=None, z_source_
     lens_mags, source_mags, lensed_source_mags = {}, {}, {}
     for band in bands:
         lens_mags[band] = slsim_lens.deflector_magnitude(band)
-        source_mags[band] = slsim_lens.extended_source_magnitude(band, lensed=False)
-        lensed_source_mags[band] = slsim_lens.extended_source_magnitude(band, lensed=True)
+        source_mags[band] = slsim_lens.extended_source_magnitude(band, lensed=False)[0]  # TODO first element
+        lensed_source_mags[band] = slsim_lens.extended_source_magnitude(band, lensed=True)[0]  # TODO first element
 
-    z_lens, z_source = slsim_lens.deflector_redshift, slsim_lens.source_redshift
+    z_lens, z_source = slsim_lens.deflector_redshift, slsim_lens.source_redshift_list[0]  # TODO confirm that first element of source_redshift_list will give the appropriate source. for galaxy-galaxy lensing, this will be the case, so this is fine for now.
     kwargs_lens = kwargs_params['kwargs_lens']
 
     # add additional necessary key/value pairs to kwargs_model
@@ -154,6 +154,13 @@ def slsim_lens_to_mejiro(slsim_lens, bands, cosmo, snr=None, uid=None, z_source_
     kwargs_model['cosmo'] = cosmo
     kwargs_model['z_source'] = z_source
     kwargs_model['z_source_convention'] = z_source_convention
+
+    # from pprint import pprint
+    # pprint(f'{kwargs_model=}')
+    # pprint(f'{kwargs_params=}')
+    # pprint(f'{lens_mags=}')
+    # pprint(f'{source_mags=}')
+    # pprint(f'{lensed_source_mags=}')
 
     return StrongLens(kwargs_model=kwargs_model,
                       kwargs_params=kwargs_params,
