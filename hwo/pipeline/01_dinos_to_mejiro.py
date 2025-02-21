dec0d9b53b79334f4d6aa3e372445f2829f13cad
bryce-wedig <b.t.wedig@wustl.edu>
2025-02-20 21:11:20 -0600

    preliminary HWO lowest detectable subhalo mass

---
+++ hwo/pipeline/01_dinos_to_mejiro.py
@@ -0,0 +1,107 @@
+import os
+import sys
+import yaml
+from glob import glob
+from pprint import pprint
+
+import numpy as np
+import astropy.cosmology as astropy_cosmo
+import pandas as pd
+from dolphin.analysis.output import Output
+from tqdm import tqdm
+
+
+def main():
+    # enable use of local modules
+    repo_dir = '/grad/bwedig/mejiro'
+    if repo_dir not in sys.path:
+        sys.path.append(repo_dir)
+    from mejiro.lenses.strong_lens import StrongLens
+    from mejiro.utils import util
+
+    # set up directories to save output to
+    data_dir = '/data/bwedig/mejiro'
+    save_dir = os.path.join(data_dir, 'hwo', 'dinos')
+    util.create_directory_if_not_exists(save_dir)
+    util.clear_directory(save_dir)
+
+    # lists of dinos systems to process
+    Double_SLACS = ['SDSSJ0029-0055', 'SDSSJ0037-0942', 'SDSSJ0819+4534', 'SDSSJ0903+4116', 'SDSSJ0936+0913',
+                'SDSSJ0959+0410', 'SDSSJ1134+6027', 'SDSSJ1204+0358', 'SDSSJ1213+6708', 'SDSSJ1218+0830',
+                'SDSSJ1531-0105', 'SDSSJ1621+3931', 'SDSSJ1627-0053', 'SDSSJ2302-0840']
+
+    Single_SLACS = ['SDSSJ0008-0004', 'SDSSJ0252+0039', 'SDSSJ0330-0020', 'SDSSJ0728+3835', 'SDSSJ0737+3216',
+                    'SDSSJ0912+0029', 'SDSSJ1023+4230', 'SDSSJ1100+5329', 'SDSSJ1112+0826', 'SDSSJ1250+0523',
+                    'SDSSJ1306+0600', 'SDSSJ1313+4615', 'SDSSJ1402+6321', 'SDSSJ1630+4520', 'SDSSJ1636+4707',
+                    'SDSSJ2238-0754', 'SDSSJ2300+0022', 'SDSSJ2303+1422', 'SDSSJ2343-0030', 'SDSSJ2347-0005']
+
+    Double_SL2S = ['SL2SJ0208-0714', 'SL2SJ0219-0829', 'SL2SJ1427+5516']
+    Single_SL2S = ['SL2SJ0214-0405', 'SL2SJ0217-0513', 'SL2SJ0225-0454', 'SL2SJ0226-0406', 'SL2SJ0226-0420',
+                'SL2SJ0232-0408', 'SL2SJ0849-0251', 'SL2SJ0849-0412', 'SL2SJ0858-0143', 'SL2SJ0901-0259',
+                'SL2SJ0904-0059', 'SL2SJ0959+0206', 'SL2SJ1358+5459', 'SL2SJ1359+5535', 'SL2SJ1401+5544',
+                'SL2SJ1402+5505', 'SL2SJ1405+5243', 'SL2SJ1406+5226', 'SL2SJ1411+5651', 'SL2SJ1420+5630',
+                'SL2SJ2214-1807']
+
+    Double_BELLS = ['SDSSJ0801+4727', 'SDSSJ0944-0147', 'SDSSJ1234-0241', 'SDSSJ1349+3612', 'SDSSJ1542+1629',
+                    'SDSSJ1631+1854', 'SDSSJ2125+0411']
+    Single_BELLS = ['SDSSJ0151+0049', 'SDSSJ0747+5055', 'SDSSJ0830+5116', 'SDSSJ1159-0007', 'SDSSJ1215+0047',
+                    'SDSSJ1221+3806', 'SDSSJ1318-0104', 'SDSSJ1337+3620', 'SDSSJ1352+3216', 'SDSSJ1545+2748',
+                    'SDSSJ1601+2138', 'SDSSJ2303+0037']
+
+    slacs = Double_SLACS + Single_SLACS
+    sl2s = Double_SL2S + Single_SL2S
+    bells = Double_BELLS + Single_BELLS
+
+    dinos_df = pd.read_csv(os.path.join(repo_dir, 'hwo', 'data', 'dinos_i_tan_et_al_2024', 'dinos_i.csv'))
+
+    for system_name in tqdm(slacs + sl2s + bells):
+        try:
+            # get system information from CSV
+            row = dinos_df[dinos_df['Lens system'] == system_name].iloc[0]
+
+            output = Output('/nfsdata1/bwedig/dinos_i_outputs')
+            _ = output.load_output(system_name, model_id='dinos_i')
+            _ = output.plot_model_overview(lens_name=system_name, model_id='dinos_i')
+
+            # create StrongLens
+            if row['z_lens'] == '--' or row['z_source'] == '--':
+                continue
+            z_lens = float(row['z_lens'])
+            z_source = float(row['z_source'])
+
+            kwargs_lens = output.kwargs_result['kwargs_lens']
+            kwargs_lens_light = output.kwargs_result['kwargs_lens_light']
+            kwargs_source = output.kwargs_result['kwargs_source']
+
+            kwargs_params = {
+                'kwargs_lens': kwargs_lens,
+                'kwargs_lens_light': kwargs_lens_light,
+                'kwargs_source': kwargs_source
+            }
+
+            kwargs_model = {
+                'cosmo': astropy_cosmo.default_cosmology.get(),
+                'lens_light_model_list': output.model_settings['model']['lens_light'],
+                'lens_model_list': output.model_settings['model']['lens'],
+                'lens_redshift_list': [z_lens] * len(kwargs_lens),
+                'source_light_model_list': output.model_settings['model']['source_light'],
+                'source_redshift_list': [z_source] * len(kwargs_source),
+                'z_source': z_source,
+                'z_source_convention': 5.
+            }
+            assert len(kwargs_model['lens_model_list']) == len(kwargs_model['lens_redshift_list'])
+            assert len(kwargs_model['source_light_model_list']) == len(kwargs_model['source_redshift_list'])
+
+            strong_lens = StrongLens(kwargs_model=kwargs_model,
+                                    kwargs_params=kwargs_params,
+                                    band='J',
+                                    uid=system_name)
+            
+            util.pickle(os.path.join(save_dir, f'{system_name}.pkl'), strong_lens)
+        except Exception as e:
+            print(f'Error processing {system_name}: {e}')
+            continue
+
+
+if __name__ == '__main__':
+    main()
---
+++ hwo/pipeline/hwo_subhalo_detection_compare.py
@@ -0,0 +1,299 @@
+import datetime
+import matplotlib.pyplot as plt
+import multiprocessing
+import numpy as np
+import os
+import sys
+import time
+from concurrent.futures import ProcessPoolExecutor, as_completed
+from copy import deepcopy
+from pyHalo.Halos.lens_cosmo import LensCosmo
+from pyHalo.concentration_models import preset_concentration_models
+from pyHalo.single_realization import SingleHalo
+from scipy.stats import chi2
+from tqdm import tqdm
+
+
+os.nice(19)
+
+
+def process_lens(params):
+    from mejiro.analysis import stats
+    from mejiro.utils import util
+    from mejiro.synthetic_image import SyntheticImage
+    from mejiro.exposure import Exposure
+    from mejiro.plots import plot_util
+
+    (run, lens, hwo, script_config, imaging_params, subhalo_params, save_dir, image_save_dir,
+     idx_to_save) = params
+    rng_seed = script_config['rng_seed']
+    band = imaging_params['band']
+    oversample = imaging_params['oversample']
+    scene_size = imaging_params['scene_size']
+    exposure_time = imaging_params['exposure_time']
+    masses = subhalo_params['masses']
+
+    kwargs_numerics = {
+        'supersampling_factor': 3,
+        'compute_mode': 'regular',
+    }
+
+    detectable_halos = []
+    results = {}
+
+    # generate and add subhalos
+    # realization = lens.generate_cdm_subhalos()
+    # lens.add_subhalos(realization)
+
+    # calculate image position and set subhalo position
+    image_x, image_y = lens.get_image_positions(pixel_coordinates=False)
+    image_distance = np.sqrt(image_x ** 2 + image_y ** 2)
+    more_distant_image_index = np.argmax(image_distance)
+    halo_x = image_x[more_distant_image_index]
+    halo_y = image_y[more_distant_image_index]
+
+    lens_no_subhalo = deepcopy(lens)
+    synth_no_subhalo = SyntheticImage(lens_no_subhalo,
+                                        hwo,
+                                        band=band,
+                                        arcsec=scene_size,
+                                        oversample=oversample,
+                                        kwargs_numerics=kwargs_numerics,
+                                        pieces=True,
+                                        verbose=False)
+    try:
+        engine_params = {
+            'rng_seed': rng_seed,
+        }
+        exposure_no_subhalo = Exposure(synth_no_subhalo,
+                                        exposure_time=exposure_time,
+                                        engine_params=engine_params,
+                                        verbose=False)
+    except Exception as e:
+        print(f'Failed to generate exposure for StrongLens {lens.uid}: {e}')
+        return lens.uid, None, None
+
+    source_exposure = exposure_no_subhalo.source_exposure
+
+    poisson_noise = exposure_no_subhalo.poisson_noise
+    dark_noise = exposure_no_subhalo.dark_noise
+    read_noise = exposure_no_subhalo.read_noise
+
+    snr_array = np.nan_to_num(source_exposure / np.sqrt(exposure_no_subhalo.exposure))
+    snr_threshold = np.quantile(snr_array, script_config['snr_quantile'])
+    masked_snr_array = np.ma.masked_where(snr_array <= snr_threshold, snr_array)
+    mask = np.ma.getmask(masked_snr_array)
+    masked_exposure_no_subhalo = np.ma.masked_array(exposure_no_subhalo.exposure, mask=mask)
+
+    pixels_unmasked = masked_snr_array.count()
+    dof = pixels_unmasked - 3
+    rv = chi2(dof)
+    threshold_chi2 = rv.isf(0.001)
+
+    for m200 in masses:
+        mass_key = f'{int(m200)}'
+        results[mass_key] = []
+
+        chi_square_list = []
+
+        center_x = halo_x
+        center_y = halo_y
+
+        pyhalo_lens_cosmo = LensCosmo(lens.z_lens, lens.z_source)
+        astropy_class = pyhalo_lens_cosmo.cosmo
+        c_model, kwargs_concentration_model = preset_concentration_models('DIEMERJOYCE19')
+        kwargs_concentration_model['scatter'] = False
+        kwargs_concentration_model['cosmo'] = astropy_class
+        concentration_model = c_model(**kwargs_concentration_model)
+        truncation_model = None
+        kwargs_halo_model = {
+            'truncation_model': truncation_model,
+            'concentration_model': concentration_model,
+            'kwargs_density_profile': {}
+        }
+        single_halo = SingleHalo(halo_mass=m200,
+                                    x=center_x, y=center_y,
+                                    mdef='NFW',
+                                    z=lens.z_lens, zlens=lens.z_lens, zsource=lens.z_source,
+                                    subhalo_flag=True,
+                                    kwargs_halo_model=kwargs_halo_model,
+                                    astropy_instance=lens.cosmo,
+                                    lens_cosmo=pyhalo_lens_cosmo)
+        c = single_halo.halos[0].c
+
+        lens_with_subhalo = deepcopy(lens)
+        lens_with_subhalo.add_subhalos(single_halo)
+        synth = SyntheticImage(lens_with_subhalo,
+                                hwo,
+                                band=band,
+                                arcsec=scene_size,
+                                oversample=oversample,
+                                kwargs_numerics=kwargs_numerics,
+                                verbose=False)
+        try:
+            engine_params = {
+                'rng_seed': rng_seed,
+                'poisson_noise': poisson_noise,
+                'dark_noise': dark_noise,
+                'read_noise': read_noise
+            }
+            exposure = Exposure(synth,
+                                exposure_time=exposure_time,
+                                engine_params=engine_params,
+                                verbose=False)
+        except Exception as e:
+            print(f'Failed to generate exposure with subhalo for StrongLens {lens.uid}: {e}')
+            return lens.uid, None, None
+
+        masked_exposure_with_subhalo = np.ma.masked_array(exposure.exposure, mask=mask)
+        chi_square = stats.chi_square(np.ma.compressed(masked_exposure_with_subhalo),
+                                        np.ma.compressed(masked_exposure_no_subhalo))
+
+        if chi_square < 0.:
+            print(f'{lens.uid}: {chi_square=}')
+            return lens.uid, None, None
+
+        chi_square_list.append(chi_square)
+        if chi_square > threshold_chi2:
+            detectable_halos.append((lens.z_lens, m200, c))
+
+        if run == idx_to_save:
+            try:
+                _, ax = plt.subplots(2, 3, figsize=(15, 10))
+                residual = masked_exposure_with_subhalo - masked_exposure_no_subhalo
+                vmax = plot_util.get_limit(residual)
+                if vmax == 0.: vmax = 0.1
+                synth.set_native_coords()
+                coords_x, coords_y = synth.coords_native.map_coord2pix(halo_x, halo_y)
+
+                ax00 = ax[0, 0].imshow(masked_exposure_no_subhalo)
+                ax01 = ax[0, 1].imshow(masked_exposure_with_subhalo)
+                ax[0, 1].scatter(coords_x, coords_y, c='r', s=10)
+                ax02 = ax[0, 2].imshow(residual, cmap='bwr', vmin=-vmax, vmax=vmax)
+
+                ax[0, 0].set_title(f'SNR: {lens.snr:.2f}')
+                ax[0, 1].set_title(f'{m200:.2e}')
+                ax[0, 2].set_title(
+                    r'$\chi^2=$ ' + f'{chi_square:.2f}, ' + r'$\chi_{3\sigma}^2=$ ' + f'{threshold_chi2:.2f}')
+
+                synth_residual = synth.image - synth_no_subhalo.image
+                vmax_synth = plot_util.get_limit(synth_residual)
+                ax10 = ax[1, 0].imshow(synth_residual, cmap='bwr', vmin=-vmax_synth, vmax=vmax_synth)
+                ax11 = ax[1, 1].imshow(exposure.exposure)
+                ax12 = ax[1, 2].imshow(snr_array)
+
+                ax[1, 0].set_title('Synthetic Residual')
+                ax[1, 1].set_title('Full Exposure With Subhalo')
+                ax[1, 2].set_title(f'SNR Array: {pixels_unmasked} pixels, {dof} dof')
+
+                plt.colorbar(ax00, ax=ax[0, 0])
+                plt.colorbar(ax01, ax=ax[0, 1])
+                plt.colorbar(ax02, ax=ax[0, 2])
+                plt.colorbar(ax10, ax=ax[1, 0])
+                plt.colorbar(ax11, ax=ax[1, 1])
+                plt.colorbar(ax12, ax=ax[1, 2])
+
+                plt.suptitle(
+                    f'StrongLens {lens.uid}, Image Shape: {exposure.exposure.shape}')
+                plt.savefig(os.path.join(image_save_dir, f'{lens.uid}_{run}.png'))
+                plt.close()
+            except Exception as e:
+                print(e)
+
+        pvals = [rv.sf(chi) for chi in chi_square_list]
+        results[mass_key] = pvals
+
+    util.pickle(os.path.join(save_dir, f'results_{lens.uid}_{run}.pkl'), results)
+    util.pickle(os.path.join(save_dir, f'detectable_halos_{lens.uid}_{run}.pkl'), detectable_halos)
+
+    return lens.uid, results, detectable_halos
+
+
+def main():
+    start = time.time()
+
+    sys.path.append('/grad/bwedig/mejiro')
+    from mejiro.utils import util
+    from mejiro.instruments.hwo import HWO
+
+    # script configuration options
+    script_config = {
+        'snr_quantile': 0.95,
+        'num_lenses': 40,  # None
+        'num_positions': 1,
+        'rng_seed': 42,
+    }
+    subhalo_params = {
+        'masses': np.logspace(6, 12, 100),
+        'r_tidal': 0.5,
+        'sigma_sub': 0.055,
+        'los_normalization': 0.
+    }
+    imaging_params = {
+        'band': 'J',  # F106
+        'scene_size': 5,  # arcsec
+        'oversample': 1,
+        'exposure_time': 12500
+    }
+
+    # set up directories to save output to
+    data_dir = '/data/bwedig/mejiro'
+    save_dir = os.path.join(data_dir, 'hwo_subhalo_detection_compare')
+    util.create_directory_if_not_exists(save_dir)
+    util.clear_directory(save_dir)
+    image_save_dir = os.path.join(save_dir, 'images')
+    os.makedirs(image_save_dir, exist_ok=True)
+
+    # collect lenses
+    lens_list = util.unpickle_all('/data/bwedig/mejiro/hwo/dinos_best')
+    print(f'Loaded {len(lens_list)} strong lenses')
+    og_count = len(lens_list)
+    num_lenses = script_config['num_lenses']
+    if num_lenses is not None:
+        repeats = int(np.ceil(num_lenses / len(lens_list)))
+        print(f'Repeating lenses {repeats} time(s)')
+        lens_list *= repeats
+        lens_list = lens_list[:num_lenses]
+    print(f'Processing {len(lens_list)} lens(es) of {og_count}')
+
+    num_permutations = len(subhalo_params['masses']) * len(lens_list)
+    idx_to_save = np.random.randint(low=num_permutations, size=len(lens_list))
+
+    # generate instrument
+    hwo = HWO()
+
+    # generate params list and spin up processes
+    params_list = [
+        (
+            run, lens, hwo, script_config, imaging_params, subhalo_params, save_dir, image_save_dir,
+            idx_to_save[run])
+        for run, lens in enumerate(lens_list)]
+    count = len(lens_list)
+    cpu_count = multiprocessing.cpu_count()
+    process_count = cpu_count  # - config.machine.headroom_cores
+    process_count -= int(cpu_count / 2)
+    if count < process_count:
+        process_count = count
+    print(f'Spinning up {process_count} process(es) on {cpu_count} core(s)')
+
+    with ProcessPoolExecutor(max_workers=process_count) as executor:
+        futures = {executor.submit(process_lens, params): params for params in params_list}
+
+        for future in tqdm(as_completed(futures), total=len(futures)):
+            # try:
+            lens_uid, results, detectable_halos = future.result()
+                # if results is not None and detectable_halos is not None:
+                #     print(f'Processed StrongLens {lens_uid}')
+            # except Exception as e:
+            #     print(f"Error encountered: {e}")
+
+    stop = time.time()
+    execution_time = str(datetime.timedelta(seconds=round(stop - start)))
+    print(f'Execution time: {execution_time}')
+
+    execution_time_per_lens = str(datetime.timedelta(seconds=round((stop - start) / len(lens_list))))
+    print(f'Execution time per lens: {execution_time_per_lens}')
+
+
+if __name__ == '__main__':
+    main()
