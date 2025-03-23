14bedadf5edda117cb2f681fe91b986c01b3ef68
bryce-wedig <b.t.wedig@wustl.edu>
2025-03-23 16:12:59 -0500

    work on figures

--- mejiro/plots/overplot.py
+++ mejiro/plots/overplot.py
@@ -12,12 +12,12 @@ def text_boxes(ax, text_list, alpha=0.5):
 
 def source_position(ax, lens, coords, alpha=1, color='y', size=100):
     source_x, source_y = lens.get_source_pixel_coords(coords)
-    return ax.plot(source_x, source_y, marker='o', color=color, markersize=size, label='Source position', alpha=alpha)
+    return ax.plot(source_x, source_y, marker='o', color=color, markersize=size, label='Source', alpha=alpha)
 
 
 def lens_position(ax, lens, coords, alpha=1, color='r', size=100):
     lens_x, lens_y = lens.get_lens_pixel_coords(coords)
-    return ax.plot(lens_x, lens_y, marker='o', color=color, markersize=size, label='Lens position', alpha=alpha)
+    return ax.plot(lens_x, lens_y, marker='o', color=color, markersize=size, label='Lens', alpha=alpha)
 
 
 def caustics(ax, lens, coords, num_pix, delta_pix=0.11, linewidth=2, alpha=1, color='g'):
@@ -29,7 +29,7 @@ def caustics(ax, lens, coords, num_pix, delta_pix=0.11, linewidth=2, alpha=1, co
         x_caustic_list.append(x)
         y_caustic_list.append(y)
 
-    return ax.plot(x_caustic_list, y_caustic_list, label='Caustics', color=color, linewidth=linewidth, alpha=alpha)
+    return ax.plot(x_caustic_list, y_caustic_list, label='Caustic', color=color, linewidth=linewidth, alpha=alpha)
 
 
 def critical_curves(ax, lens, coords, num_pix, delta_pix=0.11, linewidth=2, alpha=1, color='b'):
@@ -41,7 +41,7 @@ def critical_curves(ax, lens, coords, num_pix, delta_pix=0.11, linewidth=2, alph
         x_critical_list.append(x)
         y_critical_list.append(y)
 
-    return ax.plot(x_critical_list, y_critical_list, label='Critical curve', color=color, linewidth=linewidth,
+    return ax.plot(x_critical_list, y_critical_list, label='Critical', color=color, linewidth=linewidth,
                    alpha=alpha)
 
 
