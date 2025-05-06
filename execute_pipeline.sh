b94771f951edec3af1dd49cff38040da23c5df9b
bryce-wedig <b.t.wedig@wustl.edu>
2025-05-06 14:19:05 -0500

    prepare to re-run pipeline

--- execute_pipeline.sh
+++ execute_pipeline.sh
@@ -29,10 +29,10 @@ echo 'Simulating images...'
 python3 pipeline/updated_scripts/04_create_exposures.py
 echo 'GalSim simulations complete.'
 
-# echo 'Generating color images...'
-# python3 pipeline/updated_scripts/05_make_rgb_exposures.py
-# echo 'Color image generation complete.'
+echo 'Generating color images...'
+python3 pipeline/updated_scripts/05_make_rgb_exposures.py
+echo 'Color image generation complete.'
 
-# echo 'Generating hdf5 file...'
-# python3 06_output.py
-# echo 'hdf5 file generation complete.'
+echo 'Generating hdf5 file...'
+python3 06_output.py
+echo 'hdf5 file generation complete.'
