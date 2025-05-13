a80277f49650d135754660220e44d960c1dc4dab
Bryce Wedig <b.t.wedig@wustl.edu>
2025-05-13 16:14:12 -0500

    all tests passing

--- tests/test_hwo_imaging/test_hwo_imaging.py
+++ tests/test_hwo_imaging/test_hwo_imaging.py
@@ -14,15 +14,12 @@ def test_hwo_imaging(strong_lens):
     hwo = HWO()
     kwargs_psf = lenstronomy_util.get_gaussian_psf_kwargs(hwo.get_psf_fwhm('J'))
 
-    # The warning is expected: we're not testing for ray-shooting accuracy here, just that the code runs for different strong lenses, so we've set the oversampling factor to 1. This is not recommended for actual science cases.
-    with pytest.warns(UserWarning,
-                      match='Supersampling factor less than 5 may not be sufficient for accurate results, especially when convolving with a non-trivial PSF'):
-        synthetic_image = SyntheticImage(strong_lens=strong_lens,
-                                        instrument=hwo,
-                                        band='J',
-                                        kwargs_numerics={'supersampling_factor': 1},
-                                        kwargs_psf=kwargs_psf,
-                                        verbose=False)
+    synthetic_image = SyntheticImage(strong_lens=strong_lens,
+                                    instrument=hwo,
+                                    band='J',
+                                    kwargs_numerics={'supersampling_factor': 1},
+                                    kwargs_psf=kwargs_psf,
+                                    verbose=False)
 
     exposure = Exposure(synthetic_image,
                         exposure_time=1000,
