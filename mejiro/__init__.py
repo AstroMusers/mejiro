61b7b4c757def9b212e5e2b5a8d3424df966122e
Bryce Wedig <b.t.wedig@wustl.edu>
2025-08-25 17:36:08 -0500

    new mplstyle file with automatic import

--- mejiro/__init__.py
+++ mejiro/__init__.py
@@ -1,2 +1,10 @@
 __author__ = "Bryce Wedig"
 __version__ = "2.0.0"
+
+
+import os
+import matplotlib.pyplot as plt
+
+
+style_path = os.path.join(os.path.dirname(__file__), 'mejiro.mplstyle')
+plt.style.use(style_path)
