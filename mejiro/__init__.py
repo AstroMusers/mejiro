a1f90ed59e3d1ddc34ac50a57a86b2e56a19c912
bryce-wedig <b.t.wedig@wustl.edu>
2025-09-16 16:15:34 -0500

    remove mplstyle auto import in init

--- mejiro/__init__.py
+++ mejiro/__init__.py
@@ -1,10 +1,2 @@
 __author__ = "Bryce Wedig"
-__version__ = "2.0.0"
-
-
-import os
-import matplotlib.pyplot as plt
-
-
-style_path = os.path.join(os.path.dirname(__file__), 'mejiro.mplstyle')
-plt.style.use(style_path)
+__version__ = "2.0.1"
