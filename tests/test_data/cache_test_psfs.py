7639db97a132dd3dee0756cb593c01d5e03256a6
Bryce Wedig <b.t.wedig@wustl.edu>
2025-09-22 16:25:09 -0500

    update psf caching script

--- tests/test_data/cache_test_psfs.py
+++ tests/test_data/cache_test_psfs.py
@@ -1,20 +1,12 @@
-import hydra
-import sys
+from mejiro.engines.stpsf_engine import STPSFEngine
 
 
-@hydra.main(version_base=None, config_path='../../config', config_name='config.yaml')
-def main(config):
-    # enable use of local packages
-    repo_dir = config.machine.repo_dir
-    if repo_dir not in sys.path:
-        sys.path.append(repo_dir)
-    from mejiro.engines import stpsf_engine
-
+def main():
     bands = ['F062', 'F087', 'F106', 'F129', 'F146', 'F158', 'F184', 'F213']
 
     for band in bands:
-        psf_id = stpsf_engine.get_psf_id(band, 1, (2048, 2048), 5, 101)
-        stpsf_engine.cache_psf(psf_id, '')
+        psf_id = STPSFEngine.get_psf_id(band, 1, (2048, 2048), 1, 41)
+        STPSFEngine.cache_psf(psf_id, '')
 
 
 if __name__ == '__main__':
