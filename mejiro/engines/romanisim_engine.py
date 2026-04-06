24a078af1e2f7cf61a4d4b1c3b05d68f068ec4ba
Bryce Wedig <b.t.wedig@wustl.edu>
2026-04-06 09:54:11 -0500

    work on romanisim engine

--- mejiro/engines/romanisim_engine.py
+++ mejiro/engines/romanisim_engine.py
@@ -1,39 +1,48 @@
 import logging
-import numpy as np
-import galsim
-from galsim import roman
-
-logger = logging.getLogger(__name__)
-from romanisim import image, parameters, catalog, psf, util, wcs, persistence
-from astropy.coordinates import SkyCoord
-from astropy import units as u
-from astropy.time import Time
-from astropy import table
-import asdf
-from astropy.modeling.functional_models import Sersic2D
-from romanisim import log
-from roman_datamodels.stnode import WfiScienceRaw, WfiImage
-import romanisim.bandpass
-from astropy.io import fits
-import warnings
 from copy import deepcopy
-from romanisim.l1 import make_l1
-from romanisim.image import make_l2, gather_reference_data, make_asdf
-from romanisim import parameters, util, ris_make_utils, wcs
-import roman_datamodels
-import roman_datamodels.maker_utils as maker_utils
+import warnings
+
+import numpy as np
 from scipy.ndimage import generic_filter
+
+from astropy import table, units as u
+from astropy.coordinates import SkyCoord
+from astropy.io import fits
+from astropy.modeling.functional_models import Sersic2D
+from astropy.time import Time
+import asdf
+
+import romanisim.bandpass
+import romanisim.image as rsim_image
+import romanisim.wcs as rsim_wcs
+import romanisim.parameters as rsim_params
+import romanisim.util as rsim_util
 
 from mejiro.engines.engine import Engine
 from mejiro.utils import roman_util
+
+logger = logging.getLogger(__name__)
 
 
 class RomanISimEngine(Engine):
 
     @staticmethod
-    def build_extra_counts(synthetic_image, exposure_time, psf=None, engine_params=defaults('Roman')):
+    def defaults(instrument_name='roman'):
+        if instrument_name.lower() == 'roman':
+            return {
+                'rng_seed': 42,
+                'sca': 1,
+                'ma_table_number': 4,
+                'date': '2027-01-01T00:00:00',
+                'coord': SkyCoord(ra=270.0 * u.deg, dec=66.0 * u.deg)
+            }
+        else:
+            Engine.instrument_not_supported(instrument_name)
+
+    @staticmethod
+    def build_extra_counts(synthetic_image, exposure_time, engine_params=defaults('Roman')):
         band = synthetic_image.band
-        sca = engine_params['sca']
+        sca = synthetic_image.instrument_params['detector']
 
         # AB zeropoint flux [electrons/s]
         flux_zeropoint = romanisim.bandpass.get_abflux(band, sca)
@@ -62,18 +71,7 @@ class RomanISimEngine(Engine):
 
 
 
-    @staticmethod
-    def defaults(instrument_name='roman'):
-        if instrument_name.lower() == 'roman':
-            return {
-                'rng_seed': 42,
-                'sca': 1,
-                'ma_table_number': 4,
-                'date': '2027-01-01T00:00:00',
-                'coord': SkyCoord(ra=270.0 * u.deg, dec=66.0 * u.deg)
-            }
-        else:
-            Engine.instrument_not_supported(instrument_name)
+    
 
     @staticmethod
     def validate_engine_params(engine_params):
