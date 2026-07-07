72d81f254f1800be45185d248fcdf1f5cea22153
bryce-wedig <b.t.wedig@wustl.edu>
2026-07-07 00:05:06 -0500

    point dataset version update script at rung 1 v_2_0
    Retarget update_dataset_version_attr from rung 0 v_1_2 to rung 1 v_2_0.

--- projects/roman_data_challenge/update_dataset_version_attr.py
+++ projects/roman_data_challenge/update_dataset_version_attr.py
@@ -1,10 +1,10 @@
 import h5py
 
-label = 'roman_data_challenge_rung_0_unlabeled'
-version = 'v_1_2'
+label = 'roman_data_challenge_rung_1'  # _unlabeled
+version = 'v_2_0'
 h5_filepath = f'/data/bwedig/mejiro/{label}/06/{label}_{version}.h5'
 
-new_dataset_version = 1.2
+new_dataset_version = 2.0
 
 with h5py.File(h5_filepath, 'r+') as f:
     old_dataset_version = f.attrs['dataset_version']
